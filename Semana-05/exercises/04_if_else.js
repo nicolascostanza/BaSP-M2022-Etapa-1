d75917645134080e211475630498fdac54ef309c
nicolas costanza <nicolasantoniocostanza@gmail.com>
2022-04-14 14:55:14 -0300

    BaSP-2022: exercise 04

---
+++ Semana-05/exercises/04_if_else.js
@@ -0,0 +1,37 @@
+// If Else
+// a) Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor es mayor o igual 
+//      que 0,5 mostrar una alerta con el mensaje “Greater than 0,5” y sino un alerta con el mensaje “Lower than 0,5”.
+// b) Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
+//     “Bebe” si la edad es menor a 2 años;
+//     “Niño” si la edad es entre 2 y 12 años;
+//     “Adolescente” entre 13 y 19 años;
+//     “Joven” entre 20 y 30 años;
+//     “Adulto” entre 31 y 60 años;
+//     “Adulto mayor” entre 61 y 75 años;
+//     “Anciano” si es mayor a 75 años.
+// a)
+var numberAleatorio;
+numberAleatorio = Math.random();
+if(numberAleatorio >= 0.5) {
+    alert('Greater than 0,5');
+}else{
+    alert('Lower than 0,5');
+}
+// b)
+var age;
+age = Math.trunc(Math.random() * 100);
+if (age < 2) {
+    alert('Bebe');
+} else if (age < 13) {
+    alert('Niño');
+} else if (age < 20) {
+    alert('Adolescente');
+} else if (age < 31) {
+    alert('Joven');
+} else if (age < 61) {
+    alert('Adulto')
+} else if (age < 76) {
+    alert('Adulto mayor');
+} else {
+    alert('Anciano');
+}
