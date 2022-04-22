6b7509967450c704ee6fda9d891d0e4cc3bfcc3a
nicolas costanza <nicolasantoniocostanza@gmail.com>
2022-04-22 15:55:52 -0300

    BaSP-W06-2022: code shortening and changes let for var

--- Semana-06/javascript/sign-up.js
+++ Semana-06/javascript/sign-up.js
@@ -1,14 +1,14 @@
-let nombre = document.getElementById("name");
-let surname = document.getElementById("surname");
-let dni = document.getElementById("dni");
-let date = document.getElementById("date");
-let phone = document.getElementById("phone");
-let address = document.getElementById("address");
-let locationsingup = document.getElementById("location");
-let postal = document.getElementById("postal");
-let email = document.getElementById("email");
-let password = document.getElementById("password");
-let passwordRepeat = document.getElementById("passwordRepeat");
+var nombre = document.getElementById("name");
+var surname = document.getElementById("surname");
+var dni = document.getElementById("dni");
+var date = document.getElementById("date");
+var phone = document.getElementById("phone");
+var address = document.getElementById("address");
+var locationsingup = document.getElementById("location");
+var postal = document.getElementById("postal");
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+var passwordRepeat = document.getElementById("passwordRepeat");
 var checkedPW;
 // events
 nombre.addEventListener("blur", nameBlur);
@@ -23,10 +23,10 @@ email.addEventListener("blur", emailBlur);
 password.addEventListener("blur", passwordBlur);
 passwordRepeat.addEventListener("blur", passwordRepeatBlur);
 // objeto
-var validation = {
-    name: false,
-    surname: false,
-}
+// var validation = {
+//     name: false,
+//     surname: false,
+// }
 
 
 // area de trabajo para las funciones 
@@ -41,175 +41,174 @@ var validation = {
 
 
 function nameBlur (){
-    let nameValue = nombre.value;
-    // console.log(typeof(nameValue));
+    var nameValue = nombre.value;
     if (nameValue.length < 4){
-        console.log("error");
-    }
-    let cont = 0
-    for (let i = 0; i < nameValue.length; i++) {
-        letra = nameValue.substring(i,i+1);
-        let ascii = letra.charCodeAt();
-
-        if ((ascii < 65) || (ascii > 90 && ascii < 97) || (ascii > 122) && (ascii != 32 && ascii != 209 && ascii != 241)) {
-            // console.log("caracter que no es letra: " + ascii);
-            cont += 1;
+        console.log("error Debe tener mas de 3 caracteres");
+    } else{
+        var cont = false
+        for (var i = 0; i < nameValue.length; i++) {
+            var letterName = nameValue.substring(i,i+1);
+            let ascii = letterName.charCodeAt();
+            if ((ascii < 65) || (ascii > 90 && ascii < 97) || (ascii > 122) && (ascii != 32 && ascii != 209 && ascii != 241)) {
+                cont = true;
+            }
+        }
+        if(cont){
+            console.log("no son solo caracteres :C ");
+        } else {
+            console.log("bien el nombre");
         }
     }
-    if(cont != 0){
-        console.log("no son solo caracteres :C ");
-        
-    }
-
 }
 
 function surnameBlur (){
-    let surnameValue = surname.value;
-    // console.log(typeof(nameValue));
+    var surnameValue = surname.value;
     if (surnameValue.length < 4){
-        console.log("error");
-    }
-    let cont = 0
-    for (let i = 0; i < surnameValue.length; i++) {
-        letra = surnameValue.substring(i,i+1);
-        let ascii = letra.charCodeAt();
-
-        if ((ascii < 65) || (ascii > 90 && ascii < 97) || (ascii > 122) && (ascii != 32 && ascii != 209 && ascii != 241)) {
-            // console.log("caracter que no es letra: " + ascii);
-            cont += 1;
+        console.log("Error. Debe contener mas de 3 caracteres");
+    } else {
+        var validateSurname = false
+        for (var i = 0; i < surnameValue.length; i++) {
+            var letraSurname = surnameValue.substring(i,i+1);
+            var asciiSurname = letraSurname.charCodeAt();
+            if ((asciiSurname < 65) || (asciiSurname > 90 && asciiSurname < 97) || (asciiSurname > 122) && (asciiSurname != 32 && asciiSurname != 209 && asciiSurname != 241)) {
+                validateSurname = true;
+            }
+        }
+        if(validateSurname){
+            console.log("Error. Debe ingresar solo Letras");
+        } else{
+            console.log("Surname correcto");
         }
     }
-    if(cont != 0){
-        console.log("no son solo caracteres :C ");
-        
-    }
-
 }
 
 function dniBlur () {
-    // HACER UN IF ADENTRO DEL OTRO IF si es menor a 8 o mayor a 8 por afuera
     dniValue = document.getElementById("dni").value;
     if(dniValue.length < 8){
-        console.log("numero menor a 8");
+        console.log("dni debe tener al menos 8 numeros");
     }else {
-        console.log("bien el dni");
-    }
-    let cont = 0
-    for (let i = 0; i < dniValue.length; i++) {
-        let numero = dniValue.substring(i,i+1);
-        if (numero != Number(numero)) {
-            console.log("es una letra");
-            cont += 1;
-        } else {
-            // console.log("es un numero");
+        var validateDni = false
+        for (let i = 0; i < dniValue.length; i++) {
+            var numDni = dniValue.substring(i,i+1);
+            var asciiDni = numDni.charCodeAt();
+            if (numDni != Number(numDni) || asciiDni == 32) {
+                validateDni = true;
+            }
         }
-    }
-    if (cont != 0){
-        console.log("hay " + cont + "letras");
+        if (validateDni){
+            console.log("hay letras");
+        } else {
+            console.log("bien ingresado el dni");
+        }
     }
 }
 
-
 function dateBlur () {
-    let dateValue = date.value;
-    let day = dateValue.substring(0,2);
-    let check1 = dateValue.substring(2,3);
-    let month = dateValue.substring(3,5);
-    let check2 = dateValue.substring(5,6);
-    let year = dateValue.substring(6,10);
-
+    var dateValue = date.value;
+    var day = dateValue.substring(0,2);
+    var check1 = dateValue.substring(2,3);
+    var month = dateValue.substring(3,5);
+    var check2 = dateValue.substring(5,6);
+    var year = dateValue.substring(6);
     day = Number(day);
     month = Number(month);
     year = Number(year);
-
-    console.log("dia " + day + " mes " + month + " year " + year);
-    
-
     if(day < 1 || day > 31){
         console.log("mal el dia");
-    }
-    if (month < 1 || month > 12) {
+    } else if (month < 1 || month > 12) {
         console.log("mal el mes");
-    }
-    if(year < 1900 || year > 2022 || year != Number(year)){
+    } else if (year < 1900 || year > 2022 || year != Number(year)) {
         console.log("mal el año");
-    }
-    if(check1 != '/' && check2 != '/'){
+    } else if(check1 != '/' || check2 != '/'){
         console.log("MAL. formato dd/mm/yyyy");
+    } else {
+        console.log("bien papaaaa");
     }
 }
 
 function phoneBlur () {
-    let phoneValue = phone.value;
+    var phoneValue = phone.value;
     if(phoneValue.length == 10){
-        console.log("bien 10 numeros");
+        var validatePhone = false
+        for (var i = 0; i < phoneValue.length; i++) {
+            var numberPhone = phoneValue.substring(i,i+1);
+            var asciiPhone = numberPhone.charCodeAt();
+            if (numberPhone != Number(numberPhone) || asciiPhone == 32) {
+                validatePhone = true;
+            }
+        }
+        if (validatePhone){
+            console.log("Error, debe contener solo numeros");
+        } else {
+            console.log("telefono ingresado bien");
+        }
     }else {
         console.log("mal, debe contener 10 numeros");
-    }
-    let cont = 0
-    for (let i = 0; i < phoneValue.length; i++) {
-        let numberPhone = phoneValue.substring(i,i+1);
-        if (numberPhone != Number(numberPhone)) {
-            console.log("no es un numero");
-            cont += 1;
-        }
-    }
-    if (cont != 0){
-        console.log("hay " + cont + "letras");
     }
 }
 
 function addressBlur () {
-    let addressValue = address.value;
+    var addressValue = address.value;
     if (addressValue.length < 5){
         console.log("error se requieren mas caracteres");
-    }
-    if (addressValue.indexOf(" ") === -1){
-        console.log("no hay espacios");
-    }
-    let letrita = 0;
-    let contNumber = 0;
-    for (let i = 0; i < addressValue.length; i++) {
-        letra = addressValue.substring(i,i+1);
-        let ascii = letra.charCodeAt();
-        if (letra == Number(letra) && ascii != 32){
-            contNumber += 1;
+    } else {
+        if (addressValue.indexOf(" ") === -1){
+            console.log("no hay espacios");
+        } else {
+            var letterAddress = 0;
+            var numberAddress = 0;
+            var spacesAddress = 0;
+            for (let i = 0; i < addressValue.length; i++) {
+                letra = addressValue.substring(i,i+1);
+                let ascii = letra.charCodeAt();
+                if (letra == Number(letra) && ascii != 32){
+                    numberAddress += 1;
+                } else if (!((ascii < 65) || (ascii > 90 && ascii < 97) || (ascii > 122) && (ascii != 209 && ascii != 241))){
+                    letterAddress += 1;
+                } else if (ascii == 32 ){
+                    spacesAddress += 1
+                }
+            }
+            if(letterAddress == 0 || numberAddress == 0){
+                console.log("Error. Debe contener letras y numeros");
+            } else if(letterAddress + numberAddress + spacesAddress == addressValue.length){
+                console.log("bien ingresado");
+            } else {
+                console.log("Error. Contiene caracteres especiales");
+            }
         }
-        if (!((ascii < 65) || (ascii > 90 && ascii < 97) || (ascii > 122) && (ascii != 209 && ascii != 241))) {
-            // console.log("caracter que no es letra: " + ascii);
-            letrita += 1;
-            
-        }
     }
-    console.log("letras: " + letrita);
-    console.log("numeros: " + contNumber);
-    
 }
 
 function locationBlur () {
-    let locationValue = locationsingup.value;
+    var locationValue = locationsingup.value;
     if (locationValue.length < 4) {
         console.log("mal, debe tener mas de 3 letras");
-    }
-    let espaces = 0
-    for (let i = 0; i < locationValue.length; i++) {
-        letra = locationValue.substring(i,i+1);
-        let ascii = letra.charCodeAt();
-
-        if (ascii === 32) {
-            // console.log("caracter que no es letra: " + ascii);
-            espaces += 1;
+    } else {
+        var spacesLocation = 0
+        var lettersLocation = 0;
+        var numberLocation = 0;
+        for (let i = 0; i < locationValue.length; i++) {
+            var letter = locationValue.substring(i,i+1);
+            var asciiLocation = letter.charCodeAt();
+            if (asciiLocation === 32) {
+                spacesLocation += 1;
+            } else if (letter == Number(letter)){
+                numberLocation += 1;
+            } else if (!((asciiLocation < 65) || (asciiLocation > 90 && asciiLocation < 97) || (asciiLocation > 122) && (asciiLocation != 209 && asciiLocation != 241))){
+                lettersLocation += 1;
+            }
         }
-    }
-    if(espaces > 0) {
-        console.log("Error. No debe contener espacio, solo caracteres alfanumericos");
+        if(spacesLocation + numberLocation + lettersLocation != locationValue.length) {
+            console.log("Error. No debe contener espacio, solo caracteres alfanumericos");
+        } else {
+            console.log("bien ingresado correctamente");
+        }
     }
 }
 
 function postalBlur () {
-    let postalValue = postal.value;
-    console.log(postalValue.length);
-    
+    var postalValue = postal.value;
     if(postalValue == Number(postalValue)){
         if( postalValue.length == 4 || postalValue.length == 5 ){
             console.log("bien ingresado los numeros");
@@ -222,7 +221,7 @@ function postalBlur () {
 }
 
 function emailBlur () {
-    let emailValue = email.value;
+    var emailValue = email.value;
     var expReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
     var respuestaExpReg = expReg.test(emailValue);
     if(respuestaExpReg){
@@ -233,54 +232,47 @@ function emailBlur () {
 }
 
 function passwordBlur () {
-    let passwordValue = password.value;
-    var letritaPW = 0;
-    var numPW = 0
-
+    var passwordValue = password.value;
+    var letterPassword = 0;
+    var numberPasdword = 0
     if (passwordValue.length > 7){
         for (let i = 0; i < passwordValue.length; i++) {
-            letra = passwordValue.substring(i,i+1);
-            let ascii = letra.charCodeAt();
-            if (letra == Number(letra) && ascii != 32){
-                numPW += 1;
+            var letterPass = passwordValue.substring(i,i+1);
+            let ascii = letterPass.charCodeAt();
+            if (letterPass == Number(letterPass) && ascii != 32){
+                numberPasdword += 1;
             }
             if (!((ascii < 65) || (ascii > 90 && ascii < 97) || (ascii > 122) && (ascii != 209 && ascii != 241))) {
-                // console.log("caracter que no es letra: " + ascii);
-                letritaPW += 1;
+                letterPassword += 1;
             }
         } 
-        
-        if (letritaPW + numPW === passwordValue.length){
-            console.log("bien solo caracteres y numeros");
-            return checkedPW = passwordValue;
+        if (letterPassword + numberPasdword === passwordValue.length){
+            console.log("BIEN");
+            // return checkedPW = password.value;
         } else {
             console.log("solo permitidos caracteres alfanumericos");
         }
     } else {
         console.log("debe ingresar al menos 8 caracteres");
-        
     }
 }
 
-function passwordRepeatBlur (checkedPW) {
-    let passwordRepeatValue = passwordRepeat.value;
-    var letritaPWR = 0;
-    var numPWR = 0
-
+function passwordRepeatBlur () {
+    var passwordRepeatValue = passwordRepeat.value;
+    var lettersPasswordRepeat = 0;
+    var numberPasswordRepeat = 0;
     if (passwordRepeatValue.length > 7){
         for (let i = 0; i < passwordRepeatValue.length; i++) {
-            letra = passwordRepeatValue.substring(i,i+1);
-            let ascii = letra.charCodeAt();
-            if (letra == Number(letra) && ascii != 32){
-                numPWR += 1;
+            var letterPasswordRepeat = passwordRepeatValue.substring(i,i+1);
+            var asciiPR = letterPasswordRepeat.charCodeAt();
+            if (letterPasswordRepeat == Number(letterPasswordRepeat) && asciiPR != 32){
+                numberPasswordRepeat += 1;
             }
-            if (!((ascii < 65) || (ascii > 90 && ascii < 97) || (ascii > 122) && (ascii != 209 && ascii != 241))) {
-                // console.log("caracter que no es letra: " + ascii);
-                letritaPWR += 1;
+            if (!((asciiPR < 65) || (asciiPR > 90 && asciiPR < 97) || (asciiPR > 122) && (asciiPR != 209 && asciiPR != 241))) {
+                lettersPasswordRepeat += 1;
             }
         } 
-        
-        if (letritaPWR + numPWR === passwordRepeatValue.length){
+        if (lettersPasswordRepeat + numberPasswordRepeat == passwordRepeatValue.length){
             console.log("bien solo caracteres y numeros");
         } else {
             console.log("solo permitidos caracteres alfanumericos");
@@ -288,12 +280,9 @@ function passwordRepeatBlur (checkedPW) {
     } else {
         console.log("debe ingresar al menos 8 caracteres");
     }
-
-    if (passwordRepeatValue === checkedPW){
-        console.log("las contraseñas coinciden :D ");
-        
-    } else {
-        console.log("las contraseñas NO COINCIDEN :c ");
-        
-    }
+    // if (passwordRepeatValue != checkedPW){
+    //     console.log("MAL las contraseñas coinciden  ");
+    // } else {
+    //     console.log("bien rey coinciden ");
+    // }
 }
